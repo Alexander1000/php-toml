3edba658f7e94ef14fb8a9c617b05c539848d506
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-02-02 08:07:16 +0300

    read file

--- lexer.c
+++ lexer.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lexer.h"
 
 struct Token* parse_tokens(char* file_name)
 {
+    FILE* hFile = fopen(file_name, "r");
+    char* buffer = (char*) malloc(sizeof(char) * 1024);
+    memset(buffer, 0, sizeof(char) * 1024);
+    size_t size = fread(buffer, sizeof(char), 1024, hFile);
+    fclose(hFile);
     return (struct Token*) 0;
 }
