5de90deea9f7c3da6d8eb16724da156a62ef7b7d
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-02-02 08:16:08 +0300

    read buffer

--- lexer.c
+++ lexer.c
@@ -3,14 +3,28 @@
 #include <string.h>
 #include "lexer.h"
 
+#define BUFFER_SIZE 1024
+
 struct List* parse_tokens(char* file_name)
 {
+    // initialize list tokens
     struct List* list = malloc(sizeof(struct List));
     memset(list, 0, sizeof(struct List));
+
+    // initialize file descriptor
     FILE* hFile = fopen(file_name, "r");
-    char* buffer = (char*) malloc(sizeof(char) * 1024);
-    memset(buffer, 0, sizeof(char) * 1024);
-    size_t size = fread(buffer, sizeof(char), 1024, hFile);
+
+    // initialize buffer
+    char* buffer = (char*) malloc(sizeof(char) * BUFFER_SIZE);
+    size_t size;
+
+    do {
+        memset(buffer, 0, sizeof(char) * BUFFER_SIZE);
+        size = fread(buffer, sizeof(char), BUFFER_SIZE, hFile);
+    } while(size == BUFFER_SIZE);
+
+    // clean up
     fclose(hFile);
+    free(buffer);
     return list;
 }
