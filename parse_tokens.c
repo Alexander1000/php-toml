5ca9a823a8df21e86190d33b8deaeb0a8bddf786
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-02-21 11:03:55 +0300

    parse array

--- parse_tokens.c
+++ parse_tokens.c
@@ -4,7 +4,50 @@
 
 zval* parse_array(struct List* curToken)
 {
-    zval* array = (zval*) malloc(sizeof(zval));
-    array_init(array);
-    return array;
+    zval* return_value = (zval*) malloc(sizeof(zval));
+    array_init(return_value);
+
+    int mode = S_PLAIN_MODE;
+
+    while (curToken != 0 && curToken->value != 0) {
+        struct Token* token = curToken->value;
+        if (token == 0) {
+            continue;
+        }
+
+        if (token->type == T_TOKEN_BRACE_OPEN) {
+            break;
+        }
+
+        if (token->type == T_TOKEN_PARAMETER_NAME) {
+            char *paramName = token->data;
+            curToken = curToken->next;
+            if (curToken == 0) {
+                continue;
+            }
+            token = (struct Token *) curToken->value;
+            if (token == 0) {
+                continue;
+            }
+            if (token->type != T_TOKEN_EQUAL) {
+                zend_error(E_WARNING, "Invalid toml-format");
+            }
+            curToken = curToken->next;
+            if (curToken == 0) {
+                continue;
+            }
+            token = (struct Token *) curToken->value;
+            if (token == 0) {
+                continue;
+            }
+            if (token->type == T_TOKEN_PARAMETER_VALUE) {
+                add_assoc_string(return_value, paramName, token->data);
+                php_printf("Setup Key: %s and Value: %s\n", paramName, token->data);
+            }
+        }
+
+        curToken = curToken->next;
+    }
+
+    return return_value;
 }
