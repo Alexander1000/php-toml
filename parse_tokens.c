7f0181ff92bf4d87ef58054c042556734158c09c
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-03-13 11:38:40 +0300

    nested arrays

--- parse_tokens.c
+++ parse_tokens.c
@@ -75,6 +75,33 @@ zval* parse_tokens(struct List** pCurToken)
                             add_assoc_zval(relativeArray, partName, nestedArray);
                         }
 
+                        hashTable = HASH_OF(nestedArray);
+
+                        zval *prop;
+                        zend_string *key;
+                        zend_long h;
+
+                        int indexed = -1;
+                        zval* lastElement = 0;
+
+                        ZEND_HASH_FOREACH_KEY_VAL(hashTable, h, key, prop) {
+                            if (key) {
+                                // key-value array
+                                indexed = 0;
+                            } else {
+                                // indexed array
+                                if (indexed == -1) {
+                                    indexed = 1;
+                                }
+
+                                lastElement = prop;
+                            }
+                        } ZEND_HASH_FOREACH_END();
+
+                        if (indexed == 1 && lastElement != 0) {
+                            nestedArray = lastElement;
+                        }
+
                         relativeArray = nestedArray;
                     } else {
                         paramName = (char*) parts->value;
@@ -180,9 +207,39 @@ zval* parse_tokens(struct List** pCurToken)
                         }
 
                         if (isLastElement == 0) {
+                            HashTable *hashTable = HASH_OF(relativeArray);
+
+                            zval *prop;
+                            zend_string *key;
+                            zend_long h;
+
+                            int indexed = -1;
+                            zval* lastElement = 0;
+
+                            ZEND_HASH_FOREACH_KEY_VAL(hashTable, h, key, prop) {
+                                if (key) {
+                                    // key-value array
+                                    indexed = 0;
+                                    php_printf("Element '%s' has key: '%s'\n", parts->value, ZSTR_VAL(key));
+                                } else {
+                                    // indexed array
+                                    if (indexed == -1) {
+                                        indexed = 1;
+                                    }
+
+                                    lastElement = prop;
+                                }
+                            } ZEND_HASH_FOREACH_END();
+
+                            php_printf("Element '%s' is indexed: %d\n", parts->value, indexed);
+
+//                            if (indexed == 1 && lastElement != 0) {
+//                                nestedArray = lastElement;
+//                            }
+
+
                             char *partName = (char *) parts->value;
                             int length = strlen(partName);
-                            HashTable *hashTable = HASH_OF(relativeArray);
                             zend_string *sParamName = zend_string_alloc(length + 1, 0);
                             memset(sParamName->val, 0, sizeof(char) * (length + 1));
                             memcpy(sParamName->val, paramName, sizeof(char) * length);
@@ -197,33 +254,7 @@ zval* parse_tokens(struct List** pCurToken)
                             }
 
                             // check nested array
-                            hashTable = HASH_OF(nestedArray);
-
-                            zval *prop;
-                            zend_string *key;
-                            zend_long h;
-
-                            int indexed = -1;
-
-                            zval* lastElement = 0;
-
-                            ZEND_HASH_FOREACH_KEY_VAL(hashTable, h, key, prop) {
-                                if (key) {
-                                    // key-value array
-                                    indexed = 0;
-                                } else {
-                                    // indexed array
-                                    if (indexed == -1) {
-                                        indexed = 1;
-                                    }
-
-                                    lastElement = prop;
-                                }
-                            } ZEND_HASH_FOREACH_END();
-
-                            if (indexed == 1 && lastElement != 0) {
-                                nestedArray = lastElement;
-                            }
+                            // hashTable = HASH_OF(nestedArray);
 
                             // todo: check array as list
                             relativeArray = nestedArray;
